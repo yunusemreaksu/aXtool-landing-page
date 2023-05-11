c2abb598849dfc950983db68aacab0764d6a8293
Yunus Emre Aksu <emre_aksu@live.com>
2023-05-11 17:25:54 +0300

    Added Brands component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import Brands from './components/brands';
+
+const BRAND_IMAGES = [
+  '/brands/image-1.png',
+  '/brands/image-2.png',
+  '/brands/image-3.png',
+  '/brands/image-4.png',
+  '/brands/image-5.png'
+];
+
 export default function Home() {
-  return <div className="text-gray-900">aXtool Landing Page</div>;
+  return (
+    <div className="text-gray-900">
+      <Brands images={BRAND_IMAGES} />
+    </div>
+  );
 }
