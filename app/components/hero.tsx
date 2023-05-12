c1970c5705ee55ca387af4e37823f69a6dcbb2b1
Emre Albayrak <emre.albayrak.96@gmail.com>
2023-05-12 14:36:00 +0300

    changed font size

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Hero() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="text-center text-[60px] font-extrabold text-black">Discover the Potential</h1>
+      <h1 className="text-center text-[48px] font-extrabold text-black">Discover the Potential</h1>
       <p className="mb-[64px] text-center text-[#6B7280]">
         aXtool helps employees reach full potential by standardizing and measuring experience.
         <br />
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Navbar() {
   return (
     <div className="px-[80px] py-[24px]">
-      <Image src={'/images/navbar/aXtool.png'} alt={'aXtool logo'} width={300} height={100} />
+      <Image src={'/images/navbar/aXtool.png'} alt={'aXtool logo'} width={450} height={200} />
     </div>
   );
 }
